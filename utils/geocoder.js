e7359c622e59ccd6a29bd6b8145c3945cd2b56b3
Oluwakemilola <oluwakemilola02@gmail.com>
2025-12-08 12:23:48 +0100

    Updated geocoder and controller

--- utils/geocoder.js
+++ utils/geocoder.js
@@ -1,9 +1,18 @@
 import NodeGeocoder from "node-geocoder";
 
 const options = {
-  provider: "openstreetmap"
+  provider: "openstreetmap",
+  httpAdapter: "https",
+  formatter: null,
+  fetchOptions: {
+    headers: {
+      "User-Agent": "AddressRegistry/1.0 (oluwakemilola02@gmail.com)"
+      // Replace email with yours
+    }
+  }
 };
 
 const geocoder = NodeGeocoder(options);
 
 export default geocoder;
+
